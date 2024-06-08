85a43545b10b59cc40155740b97990d2d4d6cb4a
Telmo Samuel <telmosamuelf@gmail.com>
2024-06-08 17:56:40 +0100

    weather

--- app/src/main/java/com/example/assistantkora/WeatherSearchActivity.java
+++ app/src/main/java/com/example/assistantkora/WeatherSearchActivity.java
@@ -1,8 +1,16 @@
 package com.example.assistantkora;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class WeatherSearchActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class WeatherSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_search);
+
+        final EditText editText = findViewById(R.id.editText);
+        ImageView openBottomSheetButton = findViewById(R.id.openBottomSheetButton);
+
+        openBottomSheetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchText = editText.getText().toString().trim();
+                if (!searchText.isEmpty()) {
+                    // Pass user input to BottomSheetFragment
+                    BottomSheetFragment bottomSheetFragment = BottomSheetFragment.newInstance(searchText);
+                    bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+                }
+            }
+        });
     }
 }
